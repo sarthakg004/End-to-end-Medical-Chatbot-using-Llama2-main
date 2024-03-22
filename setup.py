2c2d8d0ecf2581fbabcbaff497ccfafd05898a29
sarthakg004 <saarthak.gupta.mec22@itbhu.ac.in>
2024-03-22 18:25:44 +0530

    creating chatbot

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Saarthak Gupta',
+    author_email= 'saarthak.gupta.mec22@itbhu.ac.in',
+    packages= find_packages(),
+    install_requires = []
+
+)
